3bf81c61cb4693b92864d9d05291e648fbc8effe
FerDeveloperJS <ferchosoftwaredev@gmail.com>
2025-02-25 13:36:42 -0500

    Se modifica la funcion de PoblarMatriz

--- index.js
+++ index.js
@@ -2,85 +2,86 @@ const lodash = require("lodash")
 
 
 class Depredador {
-    constructor(x, y) {
-        this.x = x
-        this.y = y
-    }
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
 
-    mover() {
+  mover() {
 
-    }
+  }
 }
 
 class Presa {
-    constructor(x, y, energia) {
-        this.x = x
-        this.y = y
-        this.energia = energia
-    }
+  constructor(x, y, energia) {
+    this.x = x
+    this.y = y
+    this.energia = energia
+  }
 
-    mover() {
+  mover() {
 
-    }
+  }
 }
 
 class Planta {
-    constructor(x, y, energia) {
-        this.x = x
-        this.y = y
-        this.energia = energia
-    }
+  constructor(x, y, energia) {
+    this.x = x
+    this.y = y
+    this.energia = energia
+  }
 }
 
 
 
 
 function GenerarMatriz(n, matriz = [], fila = [], fila_idx = 0) {
-    if (matriz.length === n) {
-        return matriz
-    }
-    
-    if (fila_idx < n) {
-        fila.push("")
-        return GenerarMatriz(n, matriz, fila, fila_idx + 1)
-    }
-    
-    matriz.push(fila)
-    return GenerarMatriz(n, matriz, [], 0)
-}
-
-
-
-
-function PoblarMatrizConOrganismos(matriz, fila = 0, columna = 0) {
-    if (fila === matriz.length) {
-        return matriz  
-    }
-  
-  let NumeroAleatorio = Math.random()
-  
-  
-  if (NumeroAleatorio < 0.2) {
-    matriz[fila][columna] = "Depredador"
-
-  } else if (NumeroAleatorio < 0.4) {
-    matriz[fila][columna] = "Presa"
-    
-  } else if (NumeroAleatorio < 0.6) {
-    matriz[fila][columna] = "Planta"
-    
-  } else {
-    matriz[fila][columna] = ""
+  if (matriz.length === n) {
+    return matriz
   }
-  
-  
-    if (columna === matriz[fila].length - 1) {
-        return PoblarMatrizConOrganismos(matriz, fila + 1, 0)
-    }
-  
-  
-  return PoblarMatrizConOrganismos(matriz, fila, columna + 1)
+    
+  if (fila_idx < n) {
+    fila.push("")
+    return GenerarMatriz(n, matriz, fila, fila_idx + 1)
+  }
+    
+  matriz.push(fila)
+  return GenerarMatriz(n, matriz, [], 0)
 }
+
+
+
+
+function PoblarMatrizConOrganismos(matriz, n_organismos, organismos = ["Depredador", "Presa", "Planta"], n_ciclos = 0, organismos_idx = 0) {
+  if (n_organismos > matriz.length ** 2) {
+    return "El número de organismos debe ser menor o igual a: " + matriz.length ** 2
+  }
+
+  if (n_ciclos === n_organismos) {
+    return matriz
+  }
+
+  let filaAleatoria = lodash.random(0, matriz.length - 1)
+  let columnaAleatoria = lodash.random(0, matriz.length - 1)
+
+  if (matriz[filaAleatoria][columnaAleatoria] === "") {
+    matriz[filaAleatoria][columnaAleatoria] = organismos[organismos_idx]
+
+        
+    if (organismos_idx === organismos.length - 1) {
+      return PoblarMatrizConOrganismos(matriz, n_organismos, organismos, n_ciclos + 1, 0)
+    }
+
+    return PoblarMatrizConOrganismos(matriz, n_organismos, organismos, n_ciclos + 1, organismos_idx + 1)
+  }
+
+  return PoblarMatrizConOrganismos(matriz, n_organismos, organismos, n_ciclos, organismos_idx)
+}
+
+let matriz = GenerarMatriz(3)
+
+let matrizPoblada = PoblarMatrizConOrganismos(matriz, 8)
+console.log(matrizPoblada)
 
 
 
